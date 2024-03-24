2919dae8f5923802f90a60bd3dc6f8b228217284
Valentin <valenbian17@gmail.com>
2024-03-24 22:49:09 +0100

    Completando modulo 2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+console.log("Hoy es mi cumpleaños.");
+console.log(
+  "Lamentablemente, ando algo ajustado de dinero y solo puedo invitar a algunas bebidas."
+);
+console.log(
+  "El total de la cuenta ha sido 120€, de los cuales 18€ corresponden a las bebidas."
+);
+console.log(
+  "Al invitar a las bebidas, la cueta se reduce de modo que: 120-18="
+);
+console.log(120 - 18);
+console.log("En total somos 6, si dividimos gastos de manera equitativa");
+console.log(
+  {
+    texto: "De modo que 102 / 6 =",
+  },
+  { div: 102 / 6 },
+  {
+    solucion:
+      "La cuenta, dividida a partes iguales, corresponde a 17€ por cabeza",
+  }
+);
