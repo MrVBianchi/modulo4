2aaede5fc8852fba0f32718bcffc29277904e252
Valentin <valenbian17@gmail.com>
2024-03-25 16:52:56 +0100

    Retocando console.log

--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,7 @@ console.log(
 console.log(
   "El total de la cuenta ha sido 120€, de los cuales 18€ corresponden a las bebidas."
 );
-console.log(
-  "Al invitar a las bebidas, la cueta se reduce de modo que: 120-18="
-);
+console.log("Al invitar a las bebidas, la cueta se reduce de modo que:");
 console.log(120 - 18);
 console.log("En total somos 6, si dividimos gastos de manera equitativa");
-console.log(
-  {
-    texto: "De modo que 102 / 6 =",
-  },
-  { div: 102 / 6 },
-  {
-    solucion:
-      "La cuenta, dividida a partes iguales, corresponde a 17€ por cabeza",
-  }
-);
+console.log((120 - 18) / 6);
